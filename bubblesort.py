79fd0f2d9f3badcb7c1ba6149911d7b9d29d77fc
kricd <chdellert@gmail.com>
2018-03-29 15:05:22 +0200

    var umbenannt

--- bubblesort.py
+++ bubblesort.py
@@ -5,24 +5,25 @@ from __future__ import print_function
 import random
  
 def main():
-        liste = []
+        unsorted_list = []
         for x in range(0, 20):
-                liste.append(random.randint(1, 100))
-        sortedliste = bubblesort(liste)
-        print(sortedliste)
+                unsorted_list.append(random.randint(1, 100))
+        sorted_list = bubblesort(unsorted_list)
+        print(sorted_list)
  
  
-def bubblesort(liste):
-        for a in range(0, len(liste), 1):
-                for i in range(0, len(liste)-a-1, 1):
-                        value = liste[i]
-                        nextvalue = liste[i+1] 
-                        if liste[i]>liste[i+1]:
-                                value = liste[i]
-                                nextvalue = liste[i+1]
-                                liste[i+1] = value
-                                liste[i] = nextvalue
+def bubblesort(unsorted_list):
+        for a in range(0, len(unsorted_list), 1):
+                for i in range(0, len(unsorted_list)-a-1, 1):
+                        value = unsorted_list[i]
+                        nextvalue = unsorted_list[i+1] 
+                        if unsorted_list[i]>unsorted_list[i+1]:
+                                value = unsorted_list[i]
+                                nextvalue = unsorted_list[i+1]
+                                unsorted_list[i+1] = value
+                                unsorted_list[i] = nextvalue
                                
-        return (liste)
+        return (unsorted_list)
+
 if __name__ == "__main__":
         main()
