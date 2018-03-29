f3be8e07c74ecf848ce87152d8c24ed61d0c68dc
kricd <chdellert@gmail.com>
2018-03-29 12:19:59 +0200

    bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -1,26 +1,28 @@
-#!/usr/bin/python
-
-
+#!/usr/bin/python3
+ 
+from __future__ import print_function
+ 
+import random
+ 
 def main():
-    liste = [9, 8, 7, 1, 4, 3, 2, 6, 5]
-    print (liste)
-    print(len(liste))
-    newlist = []
-    for i in range(0, len(liste)-1):
-        tmp_i = i+1
-        print(i)
-        if liste[i] > liste[tmp_i]:
-            newlist[i] = liste[tmp_i]
-        else:
-            newlist[i] = liste[i]
-
-    print ("ERGEBNIS:")        
-    for element in newlist:
-        print (element)
-    
-
-
-
-
+        liste = []
+        for x in range(0, 20):
+                liste.append(random.randint(1, 100))
+        sortedliste = bubblesort(liste)
+        print(sortedliste)
+ 
+ 
+def bubblesort(liste):
+        for a in range(0, len(liste), 1):
+                for i in range(0, len(liste)-a-1, 1):
+                        value = liste[i]
+                        nextvalue = liste[i+1] 
+                        if liste[i]>liste[i+1]:
+                                value = liste[i]
+                                nextvalue = liste[i+1]
+                                liste[i+1] = value
+                                liste[i] = nextvalue
+                               
+        return (liste)
 if __name__ == "__main__":
-    main()
+        main()
